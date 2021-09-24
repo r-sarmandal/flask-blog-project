ed8eeb8691d143340417b50e34eba4e3ec43169d
Rohan Sarmandal <rohan.209301156@muj.manipal.edu>
2021-09-24 15:16:14 +0530

    Latest

--- commands.py
+++ commands.py
@@ -1,7 +1,9 @@
 from flask.cli import with_appcontext
-
+import click
 from flaskblog import db
+from flaskblog.models import User, Post
 
+@click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
