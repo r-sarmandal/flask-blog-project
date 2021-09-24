5750fb48f34c471cc80769e3d7af626122ae4776
Rohan Sarmandal <rohan.209301156@muj.manipal.edu>
2021-09-24 15:21:49 +0530

    Latest

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flaskblog import create_app
+from flaskblog import create_app, db
 import sys
 import logging
 
@@ -8,3 +8,5 @@ app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run()
+    with app.app_context():
+        db.create_all()
