adba56dfd278b6959156d8e4635c8e9318ff1ce5
Rohan Sarmandal <rohan.209301156@muj.manipal.edu>
2021-09-24 13:23:02 +0530

    Latest

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flaskblog import create_app
+import sys
+import logging
 
 app = create_app()
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run()
