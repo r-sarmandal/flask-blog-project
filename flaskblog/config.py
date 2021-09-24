f3ce4f0be9a60881e6c0951c8611da518aa7166c
Rohan Sarmandal <rohan.209301156@muj.manipal.edu>
2021-09-24 22:02:01 +0530

    Latest

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,9 +1,12 @@
 import os
+import re
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
